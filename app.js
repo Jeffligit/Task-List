92984688d47e0cc1120cd038eccfa956b1bc5a13
Jiahui (Jeff) Li <54647089+Jeffligit@users.noreply.github.com>
2020-07-16 00:54:58 -0400

    Added user auth and uid when creating task

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-
-var firebase = firebase.database();
+let user;
+let uid;
 
 class Task {
-    constructor(description, id) {
+    constructor(description, id, user_id) {
         this.description = description;
         this.id = id;
         this.priority = {};
-        //this.userId = userId;
+        this.user_id = user_id;
     }
 }
 // ui
@@ -14,60 +14,14 @@ class UI {
     constructor(){
         this.tasks = [];
         this.priorities = [];
-
-        firebase.ref('priorities').once('value', (prioritiesSnapshot) => {
-            const allPriorities = prioritiesSnapshot.val();
-            Object.keys(allPriorities).forEach(priorityKey => {
-                const priorityData = allPriorities[priorityKey];
-                const priority = {
-                    id: priorityKey,
-                    name: priorityData.name,
-                    color: priorityData.color
-                };
-                this.priorities.push(priority);
-            });
-
-            firebase.ref('tasks').once('value', (tasksSnapshot) => {
-                const allTasks = tasksSnapshot.val();
-                Object.keys(allTasks).forEach(taskKey => {
-                    const taskData = allTasks[taskKey];
-                    const task = new Task(taskData.description, taskKey);
-    
-                    if (taskData.priority) {
-                        const priority = this.priorities.find(priority => priority.id == taskData.priority);
-                        task.priority = priority;
-                    }
-
-                    this.tasks.push(task);
-                    const taskListElement = document.getElementById("task-list");
-                    const row = document.createElement("tr");
-                    let badgeType;
-                    if (task.priority.id == "L") {
-                        badgeType = "badge-success";
-                    } else if (task.priority.id == "M") {
-                        badgeType = "badge-warning";
-                    } else {
-                        badgeType = "badge-danger";
-                    }
-                    row.innerHTML = `
-                    <td class="text-center"> <span class="badge ${badgeType}">${task.priority.name}</span> </td>
-                    <td id="row_${task.id}" class="text-center">${task.description}</td>
-                    <td class="text-center"><a style="cursor: pointer;"><img id="${task.id}" class="edit" src="pencil.svg"/></a></td>
-                    `; 
-                    taskListElement.appendChild(row);
-                });
-            });
-        });
-
-        
         
     }
 
     addTask(description, taskPriority) {
 
-        const newTaskSnapshot = firebase.ref('tasks').push();
+        const newTaskSnapshot = firebase.database().ref('tasks').push();
         const taskId = newTaskSnapshot.key;
-        const task = new Task(description, taskId);
+        const task = new Task(description, taskId, uid);
         const priorityObj = this.priorities.find(priority => priority.id == taskPriority);
         task.priority = priorityObj;
         this.tasks.push(task);
@@ -88,10 +42,11 @@ class UI {
         <td class="text-center"><a style="cursor: pointer;"><img id="${taskId}" class="edit" src="pencil.svg"/></a></td>
         `;
         list.appendChild(row);
-        firebase.ref('tasks').child(taskId).set({
+        firebase.database().ref('tasks').child(taskId).set({
             description: task.description,
             id: task.id,
-            priority: task.priority.id
+            priority: task.priority.id,
+            user_id: task.user_id
         });
 
         document.getElementById('task').value = '';
@@ -173,10 +128,11 @@ class UI {
                 </a>
             </td>
             `;
-        firebase.ref('tasks').child(id).set({
+        firebase.database().ref('tasks').child(id).set({
             description: newDesc,
             id: id,
-            priority: newPriority
+            priority: newPriority,
+            user_id: uid
         });
 
     }
@@ -187,7 +143,7 @@ class UI {
         if (!task) return;
 
         target.parentElement.parentElement.remove();
-        firebase.ref('tasks').child(id).remove();
+        firebase.database().ref('tasks').child(id).remove();
     }
 
 }
@@ -239,7 +195,7 @@ document.getElementById("task-list").addEventListener('click', function(e){
     e.preventDefault();
 });
 
-
+//Edit priority dropdown
 priorityDropDown.addEventListener("click", (e) => {
     e.preventDefault();
     const text = document.getElementById(e.target.id).text;
@@ -254,3 +210,186 @@ priorityDropDown.addEventListener("click", (e) => {
     priorityButton.textContent = text;
     
 });
+
+//Switch to register page
+document.getElementById("register-btn").addEventListener("click", function(e) {
+    document.getElementById("login-page").setAttribute("class", "card mt-4 d-none");
+    document.getElementById("registration-page").setAttribute("class", "card mt-4 d-block");
+});
+
+//Switch back to login
+document.getElementById("back-to-login-page").addEventListener("click", function(e) {
+    document.getElementById("login-page").setAttribute("class", "card mt-4 d-block");
+    document.getElementById("registration-page").setAttribute("class", "card mt-4 d-none");
+});
+
+//Log in
+document.getElementById("login-btn").addEventListener("click", function(e) {
+    const emailElement = document.getElementById("login-email");
+    const passwordElement = document.getElementById("login-password");
+    const emailAlertElement = document.getElementById("login-email-alert");
+    const passwordAlertElement = document.getElementById("login-password-alert");
+    const email = emailElement.value;
+    const password = passwordElement.value;
+
+    firebase.auth().signInWithEmailAndPassword(email, password).then(function (e) {
+        user = firebase.auth().currentUser;
+        uid = user.uid;
+        loadTasks();
+        document.getElementById("login-page").setAttribute("class", "card mt-4 d-none");
+        document.getElementById("task-list-page").setAttribute("class", "card mt-4 d-block");
+    }).catch(function (error) {
+        if (error.code == "auth/user-not-found") {
+            emailElement.setAttribute("class", "form-control border border-danger");
+            emailAlertElement.innerHTML = "This email is not in use";
+        } else if (error.code == "auth/wrong-password") {
+            passwordElement.setAttribute("class", "form-control border border-danger");
+            passwordElement.value = "";
+            passwordAlertElement.innerHTML = error.message;
+        }
+        console.log(error);
+    });
+});
+
+//Sign up
+document.getElementById("complete-reg-btn").addEventListener("click", function(e) {
+    e.preventDefault();
+    const emailElement = document.getElementById("reg-email");
+    const passwordElement = document.getElementById("reg-password");
+    const passwordConfirmElement = document.getElementById("reg-password-confirm");
+    const passwordAlertElement = document.getElementById("password-alert");
+    const emailAlertElement = document.getElementById("reg-email-alert");
+    const passwordConfirmAlertElement = document.getElementById("confirm-password-alert");
+
+    const email = emailElement.value;
+    const password = passwordElement.value;
+    const confirmPassword = passwordConfirmElement.value;
+    // if password and confirm password is not the same
+    if (email == "" && password == "") {
+        passwordAlertElement.innerHTML = "Enter a password";
+        passwordElement.setAttribute("class", "form-control border border-danger");
+        emailAlertElement.innerHTML = "Enter an email";
+        emailElement.setAttribute("class", "form-control border border-danger");
+        return;
+    } else if (password == "") {
+        passwordAlertElement.innerHTML = "Enter a password";
+        passwordElement.setAttribute("class", "form-control border border-danger");
+        return;
+    } else if (email == "") {
+        emailAlertElement.innerHTML = "Enter an email";
+        emailElement.setAttribute("class", "form-control border border-danger");
+        return;
+    } else if (password != confirmPassword) {
+        passwordConfirmAlertElement.innerHTML = "Password does not match";
+        passwordConfirmElement.value = "";
+        passwordElement.value = "";
+        passwordElement.setAttribute("class", "form-control border border-danger");
+        return;
+    }
+
+    firebase.auth().createUserWithEmailAndPassword(email, password).then(function (e) {
+        console.log("created");
+        user = firebase.auth().currentUser;
+        uid = user.uid;
+        loadTasks();
+        document.getElementById("registration-page").setAttribute("class", "card mt-4 d-none");
+        document.getElementById("task-list-page").setAttribute("class", "card mt-4 d-block");
+    }).catch(function(error) {
+        //Short password
+        if (error.code == "auth/weak-password") {
+            passwordAlertElement.innerHTML = error.message;
+            passwordElement.setAttribute("class", "form-control border border-danger");
+            //Email is already in use or not in email form.
+        } else if (error.code == "auth/email-already-in-use" || error.code == "auth/invalid-email") {
+            emailAlertElement.innerHTML = error.message;
+            emailElement.setAttribute("class", "form-control border border-danger");
+        } 
+        console.log(error);
+    });
+});
+
+// clear up errors in sign up email when email form is clicked
+document.getElementById("reg-email").addEventListener("click", (e) => {
+    e.preventDefault();
+    document.getElementById("reg-email").setAttribute("class", "form-control");
+    document.getElementById("reg-email-alert").innerHTML = "";
+});
+
+// clear up errors in password and confirm password when password form is clicked
+document.getElementById("reg-password").addEventListener("click", (e) => {
+    e.preventDefault();
+    document.getElementById("reg-password").setAttribute("class", "form-control");
+    document.getElementById("reg-password-confirm").setAttribute("class", "form-control");
+    document.getElementById("password-alert").innerHTML = "";
+    document.getElementById("confirm-password-alert").innerHTML = "";
+});
+
+// clear up errors in password and confirm password when confirm password form is clicked
+document.getElementById("reg-password-confirm").addEventListener("click", (e) => {
+    e.preventDefault();
+    document.getElementById("reg-password").setAttribute("class", "form-control");
+    document.getElementById("reg-password-confirm").setAttribute("class", "form-control");
+    document.getElementById("password-alert").innerHTML = "";
+    document.getElementById("confirm-password-alert").innerHTML = "";
+});
+
+// clear up errors in login email when email form is clicked
+document.getElementById("login-email").addEventListener("click", (e) => {
+    e.preventDefault();
+    document.getElementById("login-email").setAttribute("class", "form-control");
+    document.getElementById("login-email-alert").innerHTML = "";
+});
+
+// clear up errors in login password when password form is clicked
+document.getElementById("login-password").addEventListener("click", (e) => {
+    e.preventDefault();
+    document.getElementById("login-password").setAttribute("class", "form-control");
+    document.getElementById("login-password-alert").innerHTML = "";
+});
+
+function loadTasks() {
+    firebase.database().ref('priorities').once('value', (prioritiesSnapshot) => {
+        const allPriorities = prioritiesSnapshot.val();
+        Object.keys(allPriorities).forEach(priorityKey => {
+            const priorityData = allPriorities[priorityKey];
+            const priority = {
+                id: priorityKey,
+                name: priorityData.name,
+                color: priorityData.color
+            };
+            ui.priorities.push(priority);
+        });
+
+        firebase.database().ref('tasks').once('value', (tasksSnapshot) => {
+            const allTasks = tasksSnapshot.val();
+            Object.keys(allTasks).forEach(taskKey => {
+                const taskData = allTasks[taskKey];
+                const task = new Task(taskData.description, taskKey);
+
+                if (taskData.priority) {
+                    const priority = ui.priorities.find(priority => priority.id == taskData.priority);
+                    task.priority = priority;
+                }
+
+                ui.tasks.push(task);
+                const taskListElement = document.getElementById("task-list");
+                const row = document.createElement("tr");
+                let badgeType;
+                if (task.priority.id == "L") {
+                    badgeType = "badge-success";
+                } else if (task.priority.id == "M") {
+                    badgeType = "badge-warning";
+                } else {
+                    badgeType = "badge-danger";
+                }
+                row.innerHTML = `
+                <td class="text-center"> <span class="badge ${badgeType}">${task.priority.name}</span> </td>
+                <td id="row_${task.id}" class="text-center">${task.description}</td>
+                <td class="text-center"><a style="cursor: pointer;"><img id="${task.id}" class="edit" src="pencil.svg"/></a></td>
+                `; 
+                taskListElement.appendChild(row);
+            });
+        });
+    });
+
+}
